e304b0732cc15bcffffa73dbdaf08fc68340225f
Alex <alex123joha@gmail.com>
2021-03-06 18:24:10 -0500

    Val bet

--- src/main/java/com/example/demo/controllers/BetController.java
+++ src/main/java/com/example/demo/controllers/BetController.java
@@ -5,8 +5,9 @@ import com.example.demo.services.BetService;
 import com.example.demo.services.RouletteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/bet")
@@ -16,40 +17,52 @@ public class BetController {
     @Autowired
     RouletteService rouletteService;
     @PostMapping("/create")
-    public String createBet(@RequestParam Long userId, @RequestParam double amount, @RequestParam(required = false) String color, @RequestParam(required = false) Integer num, @RequestParam Long rouletteId){
-        RouletteModel roulette = rouletteService.findById(rouletteId);
-        BetModel bet = new BetModel(userId, amount, color, num);
+    public String createBet(@RequestHeader(value = "userId") Long userId, @RequestBody BetModel bet){
+        Long rouletteId = bet.getRoulette().getId();
+        bet.setRoulette(rouletteService.findById(rouletteId));
+        bet.setUserId(userId);
+        String[] resVal = valParams(bet).split(",",2);
+        if(resVal[0].equals("OK")){
+            betService.saveBet(bet);
+
+            return resVal[1];
+        }
+
+        return resVal[1]; 
+    } 
+    public String valParams(BetModel bet){
+        String color = bet.getColor();
+        Integer num = bet.getNum();
+        double amount = bet.getAmount();
+        RouletteModel roulette = bet.getRoulette();
         if(roulette == null)
 
-            return "No se puede realizar la apuesta, la ruleta no existe";
+            return "NO,No se puede realizar la apuesta, la ruleta no existe";
         if(roulette.getStatus().equals("cerrada"))
 
-            return "No se puede realizar la apuesta, la ruleta se encuentra cerrada";
+            return "NO,No se puede realizar la apuesta, la ruleta se encuentra cerrada";
         if(color == null && num == null)
 
-            return "No se puede realizar la apuesta sin un numero o color";
+            return "NO,No se puede realizar la apuesta sin un numero o color";
         if(color != null && num != null)
 
-            return "La apuesta no se puede realizar a numero y color al mismo tiempo";
-        if(amount < 0 || amount > 10000)
+            return "NO,La apuesta no se puede realizar a numero y color al mismo tiempo";
+        if(amount <= 0 || amount > 10000)
 
-            return "No se puede realizar la apuesta, el monto debe estar entre 0 y 10000";
+            return "NO,No se puede realizar la apuesta, el monto debe estar entre 0 y 10000";
         if(num != null){
-            if(num < 0 || num > 36)
+            if(num < 0 || num > 36){
 
-                return "No se puede realizar la apuesta, el numero debe estar entre 0 y 36";
-            bet.setRoulette(roulette);
-            betService.saveBet(bet);
+                return "NO,No se puede realizar la apuesta, el numero debe estar entre 0 y 36";
+            }
 
-            return "Apuesta realizada en la ruleta con id: " + rouletteId;
+            return "OK,Apuesta realizada en la ruleta con id: " + bet.getRoulette().getId();
         }
         if(color.toLowerCase().equals("rojo") || color.toLowerCase().equals("negro")){
-            bet.setRoulette(roulette);
-            betService.saveBet(bet);
 
-            return "Apuesta realizada en la ruleta con id: " + rouletteId;
+            return "OK,Apuesta realizada en la ruleta con id: " + bet.getRoulette().getId();
         }
 
-        return "No se puede realizar la apuesta, el color debe ser rojo o negro";
-    } 
+        return "NO,No se puede realizar la apuesta, el color debe ser rojo o negro";
+    }
 }
